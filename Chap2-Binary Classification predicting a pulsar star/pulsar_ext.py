35ff307ac1c280b34f334d3635a478010f96ef96
Jinsung Jeon <jjsjjs0902@naver.com>
2020-01-21 15:57:19 +0900

    update

--- Chap2-Binary Classification predicting a pulsar star/pulsar_ext.py
+++ Chap2-Binary Classification predicting a pulsar star/pulsar_ext.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 21 14:48:34 2020
+Created on Tue Jan 21 15:31:26 2020
 
 @author: Jinsung
 """
@@ -18,23 +18,39 @@ RND_STD = 0.0030
 LEARNING_RATE = 0.001
 
 #실험용 메인 함수 정의
-def pulsar_exec(epoch_count=10, mb_size=10, report=1):
-    load_pulsar_dataset()
-    init_model() #파라미터 초기화
+def pulsar_exec(epoch_count=10, mb_size=10, report=1, adjust_ratio = False):
+    load_pulsar_dataset(adjust_ratio)   
+    init_model()
     train_and_test(epoch_count, mb_size, report)
     
 #데이터 적재 함수 정의
-def load_pulsar_dataset():
+def load_pulsar_dataset(adjust_ratio):
     with open('C:\\Users\\Jinsung\\Documents\\Deep_Learning_Code\\Datasets\\predicting-a-pulsar-star\\pulsar_stars.csv') as csvfile:
         csvreader = csv.reader(csvfile)
-        next(csvreader, None) #첫 행은 읽지 않고 건너뛴다
-        rows = []
+        next(csvreader, None) 
+        pulsars, stars = [], [] #pulsars와 stars 따로 나눠줌
         for row in csvreader:
-            rows.append(row)
+            if row[8] == '1':
+                pulsars.append(row)
+            else:
+                stars.append(row)
         
         global data, input_cnt, output_cnt
         input_cnt, output_cnt = 8, 1
-        data = np.asarray(rows, dtype='float32')
+        
+        star_cnt, pulsar_cnt = len(stars), len(pulsars)
+        
+        #adjust_ratio = true 일때 pulsar를 star의 개수와 같아지도록 data에 삽입
+        if adjust_ratio:
+            data = np.zeros([2*star_cnt, 9])
+            data[0:star_cnt, :] = np.asarray(stars, dtype='float32')
+            for n in range(star_cnt):
+                data[star_cnt+n] = np.asarray(pulsars[n%pulsar_cnt], dtype='float32')
+        else:
+            data = np.zeros([star_cnt+pulsar_cnt, 9])
+            data[0:star_cnt, :] = np.asarray(stars, dtype='float32')
+            data[star_cnt:, :] = np.asarray(pulsars, dtype='float32')
+        
             
 #파라미터 초기화 함수 정의
 def init_model():
@@ -46,22 +62,23 @@ def init_model():
 def train_and_test(epoch_count, mb_size, report):
     step_count = arrange_data(mb_size)
     test_x, test_y = get_test_data()
-    
+    print('\nresult = accuracy, precision, recall, f1')
     for epoch in range(epoch_count):
-        losses, accs = [], []
+        losses = []
         
         for n in range(step_count):
             train_x, train_y = get_train_data(mb_size, n)
             loss, acc = run_train(train_x, train_y)
             losses.append(loss)
-            accs.append(acc)
             
         if report > 0 and (epoch+1) % report == 0:
             acc = run_test(test_x, test_y)
-            print('Epoch {}: loss ={:5.3f}, accuracy ={:5.3f}/{:5.3f}'.format(epoch+1, np.mean(losses), np.mean(accs),acc))
-        
-    final_acc = run_test(test_x, test_y)
-    print('\nFinal Test: final accuracy = {:5.3f}'.format(final_acc))
+            acc_str = ','.join(['%5.3f']*4) % tuple(acc)
+            print('Epoch {}: loss ={:5.3f}, result ={}'.format(epoch+1, np.mean(losses), acc_str))
+    
+    acc = run_test(test_x, test_y)
+    acc_str = ','.join(['%5.3f']*4) % tuple(acc)
+    print('\nFinal Test: final result = {}'.format(acc_str))
     
 #학습 및 평가 데이터 획득 함수 정의
 def arrange_data(mb_size):
@@ -137,11 +154,28 @@ def backprop_postproc(G_loss, aux):
 
 #정확도 계산 함수 정의
 def eval_accuracy(output, y):
-    estimate = np.greater(output, 0)
-    answer = np.greater(y, 0.5)
-    correct = np.equal(estimate, answer) #같으면 1 다르면 0
+    est_yes = np.greater(output,0)
+    ans_yes = np.greater(y, 0.5)
+    est_no = np.logical_not(est_yes)
+    ans_no = np.logical_not(ans_yes)
     
-    return np.mean(correct)
+    tp = np.sum(np.logical_and(est_yes, ans_yes))
+    fp = np.sum(np.logical_and(est_yes, ans_no))
+    fn = np.sum(np.logical_and(est_no, ans_yes))
+    tn = np.sum(np.logical_and(est_no, ans_no))
+    
+    accuracy = safe_div(tp+tn, tp+tn+fp+fn)
+    precision = safe_div(tp, tp+fp)
+    recall = safe_div(tp, tp+fn)
+    f1 = 2*safe_div(recall*precision, recall+precision)
+    
+    return [accuracy, precision, recall, f1]
+
+def safe_div(p, q):
+    p, q = float(p), float(q)
+    if np.abs(q)<1.0e-20:
+        return np.sign(p)
+    return p/q
 
 #시그모이드 관련 함수 정의
 def relu(x):
@@ -160,3 +194,4 @@ def sigmoid_cross_entropy_with_logits_derv(z, x):
     return -z + sigmoid(x)
 
 pulsar_exec()
+pulsar_exec(adjust_ratio=True)
